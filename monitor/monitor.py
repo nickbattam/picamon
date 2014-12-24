e5322bc79aad0de31d8056bfda6eda75e2980a64
Pi <pi@diamond.ac.uk>
2014-12-24 18:18:30 +0000

    started on creating a test image from monitor class

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -4,9 +4,14 @@
 # 1. monitor PV corresponding to specific raspi
 # 2. 
 
+import numpy as np
+
 class Monitor:
 	''' monitor class to listen for broadcast insutrctions
   from network with pv name and camera to stream from '''
 
-	def __init(self):
+	def __init__(self):
 		self.staticip = "192.168."
+
+	def testimage(self, x=100, y=100):
+		return np.arange(x*y).reshape(x,y)
--- monitor/testharness.py
+++ monitor/testharness.py
@@ -4,8 +4,19 @@ import unittest
 import monitor
 
 class TestMonitor(unittest.TestCase):
+
 	def test_test(self):
-		print "hellod world"
+		print "hello world"
+
+	def test_attr(self):
+		mon = monitor.Monitor()
+		assert mon.staticip!=""
+
+	def test_testimage(self):
+		mon = monitor.Monitor()
+		image = mon.testimage()
+		assert len(image) != 0
+
 
 if __name__ == '__main__':
 	unittest.main()
