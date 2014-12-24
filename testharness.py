9bd5b31893b1f4cb8b1725570efaa230a405e7ba
Pi <pi@diamond.ac.uk>
2014-12-24 17:54:58 +0000

    Monitor code and test harness, this time with tracked files!

---
+++ testharness.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import unittest
+import monitor
+
+class TestMonitor(unittest.TestCase):
+	def test_test(self):
+		print "hellod world"
+
+if __name__ == '__main__':
+	unittest.main()
+
