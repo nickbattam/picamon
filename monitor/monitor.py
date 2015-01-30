d3c9f3b195b1cb19d0651759a12d16b186407f08
Nick Battam <nicholas.battam@diamond.ac.uk>
2015-01-30 17:38:30 +0000

    Initial version of a camera object

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -6,14 +6,24 @@
 
 import numpy as np
 import epics
+from epics import pv
 
-class Monitor:
+from camera import Camera
+
+class Monitor(object):
 	''' monitor class to listen for broadcast insutrctions
   from network with pv name and camera to stream from '''
 
 	def __init__(self):
 		self.pvprefix = "MON-CONTROL:"
 		self.monitorname = "PI1"
+
+		self.camera_name = pv.PV(self.pvprefix + self.monitorname,
+			callback=self.update_camera)
+
+	def update_camera(self, value, **kw):
+		self.camera.close()
+		self.camera = Camera(value)
 
 	def testimage(self, x=100, y=100):
 		return np.arange(x*y).reshape(x,y)
@@ -24,3 +34,4 @@ class Monitor:
   # camonitor the cameraname to know which camera to read
   # camonitor $(PREFIX):CAM1:arrayDate to get image stream
 	# camonitor $(PREFIX):CAM1:xsize/ysize to get image size
+
