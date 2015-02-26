e2d045b64dc01d78b1f9cdcf4bbc9026e5f9234a
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2015-02-26 14:15:14 +0000

    basic init and show methods for plotter class

--- monitor/plotter.py
+++ monitor/plotter.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env dls-python
+import matplotlib.pyplot as plt
 
+class Plotter:
+
+    def __init__(self):
+        plt.ion()
+        plt.axis('off')
+
+    def show(data,xsize,ysize):
+        plt.show(data)
