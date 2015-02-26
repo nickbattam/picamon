6c14dc80e551b43c561c9a0c3b9b5c29894b0db4
Nick Battam <nicholas.battam@diamond.ac.uk>
2015-02-26 15:52:08 +0000

    First pass at migrating pygame_example into callable plotting routine
    (this is unfinished and untried)

--- monitor/plotter_pygame.py
+++ monitor/plotter_pygame.py
@@ -1,1 +1,26 @@
-__author__ = 'nick'
+import pygame
+
+DEFAULT_XSIZE = 500
+DEFAULT_YSIZE = 500
+
+
+class PyGamePlotter(object):
+
+    def __init__(self):
+        pygame.init()
+
+        self._screen_size = (DEFAULT_XSIZE, DEFAULT_YSIZE)
+        self._screen = pygame.display.set_mode(self._screen_size)
+
+    def show(self, data, xsize, ysize):
+        screen_size = (xsize, ysize)
+        if screen_size != self._screen_size:
+            self._screen = pygame.display.set_mode(screen_size)
+
+        pygame.surfarray.blit_array(self._screen, data)
+        pygame.display.flip()
+
+
+
+
+
