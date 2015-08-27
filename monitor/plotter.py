f124c967a4f2d05b318ec61893cecabf6185d48d
Arvinder Palaha <arvinder.palaha@diamond.ac.uk>
2015-08-27 13:19:03 +0100

    remove interpolation from plotted data (pixels seen as actual pixels) set variable in class to access matplotlib figure object

--- monitor/plotter.py
+++ monitor/plotter.py
@@ -5,11 +5,12 @@ class Plotter:
 
     def __init__(self):
         plt.ion()
+        self._screen = plt.figure()
         plt.axis('off')
 
     def set_image_size(self, xsize, ysize):
         pass
 
     def show(self, data):
-        plt.imshow(data)
+        plt.imshow(data, interpolation='None')
 
