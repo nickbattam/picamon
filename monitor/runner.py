58f982d01a7c47a12a7ae600c2ca17cb6c5c7ed9
Nick Battam <nicholas.battam@diamond.ac.uk>
2015-11-19 13:25:21 +0000

    Update to set screensize and take camera IOC as arg

--- monitor/runner.py
+++ monitor/runner.py
@@ -1,19 +1,24 @@
+import sys
 from time import sleep
-from monitor.camera import Camera
-from monitor.plotter_pygame import PyGamePlotter
+from camera import Camera
+from plotter_pygame import PyGamePlotter
 
 
 def run(plotter, camera):
-
+    old_timestamp = -1
     while True:
-        plotter.show(camera.get_image_data())
+        data, timestamp = camera.get_image_data()
+        if timestamp != old_timestamp:
+            plotter.show(data)
+            old_timestamp = timestamp
         sleep(1.0)
 
-if __name__ == "main":
+if __name__ == "__main__":
 
-    cam_ioc = "X1-CAM"
+    cam_ioc = sys.argv[1]  # "X1-CAM"
 
-    plo = PyGamePlotter()
     cam = Camera(cam_ioc)
+    plo = PyGamePlotter()
+    plo.set_screensize(cam.xsize, cam.ysize)
 
     run(plo, cam)
