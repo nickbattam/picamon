2c47e3d76d87233f99a208125f7c7620e1c12a83
David Michel <dmichel76@googlemail.com>
2016-01-22 14:13:01 +0000

    udpated controller IOC to separate databases with more appropriate names to separate general records to the one that apply only to a single monitor

--- controller/iocBoot/iocpicamon/st.cmd
+++ controller/iocBoot/iocpicamon/st.cmd
@@ -13,7 +13,9 @@ picamon_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=nickHost")
-dbLoadRecords "db/dbController.db", "p=MON-CONTROL, monitor=PI1, camera=CAM1"
+dbLoadRecords "db/general.db", "p=MON-CONTROL"
+dbLoadRecords "db/monitor.db", "p=MON-CONTROL, monitor=PI1"
+dbLoadRecords "db/monitor.db", "p=MON-CONTROL, monitor=PI2"
 
 
 cd ${TOP}/iocBoot/${IOC}
