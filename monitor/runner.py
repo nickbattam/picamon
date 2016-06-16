17793c9b3ceecc206aab1d1c34c0d3dc69892cbd
Nick Battam <nicholas.battam@diamond.ac.uk>
2016-06-16 13:32:18 +0100

    Use ArgumentParser to enforce required arguments

--- monitor/runner.py
+++ monitor/runner.py
@@ -6,16 +6,13 @@ from plotter_pygame import PyGamePlotter
 import epics
 import argparse
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--prefix', dest='prefix', help='controller IOC prefix')
-    parser.add_argument('--name', dest='name', help='name of monitor')
+    parser.add_argument('--prefix', required=True, dest='prefix', help='controller IOC prefix')
+    parser.add_argument('--name', required=True, dest='name', help='name of monitor')
     parser.add_argument('--fullscreen', dest='fullscreen', default=1, help='1 for fullscreen (default), 0 for small window')
     args = parser.parse_args()
-
-    if not (args.prefix and args.name):
-        parser.error("Arguments missing. Please use both --prefix and --name")
 
     controller = Controller(args.prefix, args.name)
     plotter = PyGamePlotter(args.name, args.fullscreen)
