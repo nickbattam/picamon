69f2e7fb1d867a015a61028ec568f190936aa1b5
David Michel <micd@tessella.co.uk>
2018-01-04 09:51:40 +0000

    fixed issues when using the same PV twice with different PV object -> confused callbacks for pyepics

--- ui/python/picamon.py
+++ ui/python/picamon.py
@@ -12,16 +12,14 @@ class PVWidget():
         d['default'] = args.pop('default', PVWidget.DEFAULT)
         return d
 
-    def __init__(self, pvname, default=DEFAULT):
-        self.pvname = pvname
+    def __init__(self, pv, default=DEFAULT):
         self.default = default
         self.value = default
         self.stringvalue = str(default)
-        self.pv = epics.PV(pvname)
+        self.pv = epics.PV(pv) if isinstance(pv, basestring) else pv
         self.pv.add_callback(self.onPVChanges)
 
     def onPVChanges(self, pvname=None, value=None, char_value=None, **kw):
-        #print "onPVChanges"
         self.value = value
         self.stringvalue = ', '.join(map(str, self.value)) if self.pv.nelm > 1 else str(self.value)
         wx.CallAfter(self.updateContent)
@@ -30,14 +28,13 @@ class PVWidget():
 
     def getValue(self): return self.value
     def getPV(self): return self.pv
-    def getPVname(self): return self.pvname
 
 
 class PVTextUpdate(wx.StaticText, PVWidget):
     
-    def __init__(self, parent, pvname, **kw):
+    def __init__(self, parent, pv, **kw):
         cargs = PVWidget.processArgs(kw)
-        PVWidget.__init__(self, pvname, default=cargs['default'])
+        PVWidget.__init__(self, pv, default=cargs['default'])
         wx.StaticText.__init__(self, parent, id=wx.ID_ANY, **kw)
         self.SetLabel(self.default)
        
@@ -46,10 +43,10 @@ class PVTextUpdate(wx.StaticText, PVWidget):
 
 class PVDropdownList(wx.Choice, PVWidget):
 
-    def __init__(self, parent, pvname, writetopv=None, condition=lambda x:True, onSelection=None, **kw):
+    def __init__(self, parent, pv, writetopv=None, condition=lambda x: x!="", onSelection=None, **kw):
 
         cargs = PVWidget.processArgs(kw)
-        PVWidget.__init__(self, pvname, default=cargs['default'])
+        PVWidget.__init__(self, pv, default=cargs['default'])
         wx.Choice.__init__(self, parent, id=wx.ID_ANY, **kw)
         self.condition = condition
         self.writetopv = writetopv
@@ -58,13 +55,11 @@ class PVDropdownList(wx.Choice, PVWidget):
         else: self.Bind(wx.EVT_CHOICE, self.onSelection)
 
     def updateContent(self):
-        #print "update content"
         wx.Choice.Clear(self)       
-        for choice in list(filter(lambda x: x!="", self.value)): wx.Choice.Append(self, str(choice))
+        for choice in list(filter(self.condition, self.value)): wx.Choice.Append(self, str(choice))
 
     def onSelection(self, event):
-        #print "onselection"
-        if self.writetopv: epics.PV(self.writetopv).put(self.GetStringSelection())
+        if self.writetopv: epics.PV(self.writetopv).put(event.GetEventObject().GetStringSelection())
 
     def setWriteToPV(self, writetopv):
         self.writetopv = writetopv
@@ -75,33 +70,37 @@ class PiCaMonApp(wx.Frame):
     def __init__(self, prefix):
 
         self.prefix = prefix
-        self.selectedMonitor = None
 
+        # prepare frame + main panel
         wx.Frame.__init__(self, None, wx.ID_ANY, "PiCaMon", size=(300, 150))
         panel = wx.Panel(self, wx.ID_ANY) 
+
+        # create PV objects
+        monitors_pv = epics.PV("%s:LIST:MONITOR" % self.prefix)
+        cameras_pv = epics.PV("%s:LIST:CAMERA" % self.prefix)
+
+        # create wx widgets
+        self.monitors = PVDropdownList(panel, monitors_pv, onSelection=self.onMonitorSelection)
+        self.cameras = PVDropdownList(panel, cameras_pv)
+
+        # place in UI layout
         vbox = wx.BoxSizer(wx.VERTICAL)
-
-        monitors = epics.caget("%s:LIST:MONITOR" % self.prefix)
-
+        
         # display the list of monitor/camera pairs
-        for monitor in list(filter(lambda x: x!="", monitors)):
+        for monitor in list(filter(lambda x: x!="", monitors_pv.get())):
             hbox = wx.BoxSizer(wx.HORIZONTAL)
             st = wx.StaticText(panel, label = monitor, style = wx.ALIGN_CENTRE)
-            hbox.Add(st, 0, wx.ALL|wx.CENTER, 5)
-            tu = PVTextUpdate(panel, "%s:%s:CAMERA" % (self.prefix,monitor))   
+            hbox.Add(st, 0, wx.ALL|wx.CENTER, 5) 
+            tu = PVTextUpdate(panel, "%s:%s:CAMERA" % (self.prefix, monitor))  
             hbox.Add(tu, 0, wx.ALL|wx.CENTER, 5)
             vbox.Add(hbox, 1, wx.ALL|wx.EXPAND)
 
         # dropdown lists to select monitor/camera. 
         hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.monitors = PVDropdownList(panel, "%s:LIST:MONITOR" % self.prefix, onSelection=self.onMonitorSelection, condition=lambda x: x!="")
         hbox2.Add(self.monitors, 0, wx.ALL|wx.CENTER, 5)
-
-        self.cameras = PVDropdownList(panel, "%s:LIST:CAMERA" % self.prefix, condition=lambda x: x!="")
         hbox2.Add(self.cameras, 0, wx.ALL|wx.CENTER, 5)
-
         vbox.Add(hbox2, 1, wx.ALL|wx.EXPAND)
+        
         panel.SetSizer(vbox)    
 
     def onMonitorSelection(self, event):
