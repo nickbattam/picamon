deb9e0bd8a7260f812e1a4267bebd64ea20ffc23
David Michel <micd@tessella.co.uk>
2018-01-08 20:28:53 +0000

    new picamon client using pyepics.wx support

--- ui/python/picamon.py
+++ ui/python/picamon.py
@@ -1,122 +1,75 @@
+import sys
 import wx
-import epics
-import time
+import epics.wx
 
-class PVWidget():
+prefix = "D100X"
 
-    DEFAULT = "..."
+def onCameraSelection(event):
+    widget = event.GetEventObject()
+    camera = widget.GetStringSelection()
+    monitor = widget.GetName()
+    epics.caput("%s:%s:CAMERA" % (prefix, monitor),"%s" % camera)
 
-    @staticmethod
-    def processArgs(args):
-        d = {}
-        d['default'] = args.pop('default', PVWidget.DEFAULT)
-        return d
+app = wx.App()
 
-    def __init__(self, pv, default=DEFAULT):
-        self.default = default
-        self.value = default
-        self.stringvalue = str(default)
-        self.pv = epics.PV(pv) if isinstance(pv, basestring) else pv
-        self.pv.add_callback(self.onPVChanges)
+bigfont = wx.Font(24, wx.DECORATIVE, wx.NORMAL, wx.BOLD)
+boldfont = wx.Font(12, wx.DECORATIVE, wx.NORMAL, wx.BOLD)
+normalfont =  wx.Font(12, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+smallfont =  wx.Font(8, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
 
-    def onPVChanges(self, pvname=None, value=None, char_value=None, **kw):
-        print "onPVChanges"
-        self.value = value
-        self.stringvalue = ', '.join(map(str, self.value)) if self.pv.nelm > 1 else str(self.value)
-        wx.CallAfter(self.updateContent)
-       
-    def updateContent(self): pass
+frame = wx.Frame(None, -1, 'PiCaMon', size=(300, 220))
+panel = wx.Panel(frame, -1)
+vbox = wx.BoxSizer(wx.VERTICAL)
 
-    def getValue(self): return self.value
-    def getPV(self): return self.pv
+#header with name and logo
+header = wx.BoxSizer(wx.HORIZONTAL)
 
+name = wx.StaticText(panel, label = "PiCaMon", style = wx.ALIGN_CENTRE)
+name.SetFont(bigfont)
+header.Add(name, wx.ALL|wx.CENTER, 5)
 
-class PVTextUpdate(wx.StaticText, PVWidget):
-    
-    def __init__(self, parent, pv, **kw):
-        cargs = PVWidget.processArgs(kw)
-        PVWidget.__init__(self, pv, default=cargs['default'])
-        wx.StaticText.__init__(self, parent, id=wx.ID_ANY, **kw)
-        self.SetLabel(self.default)
-       
-    def updateContent(self): 
-        self.SetLabel(self.stringvalue)
+img = wx.Bitmap.ConvertToImage(wx.Bitmap("./picamon.png")).Scale(75,75, wx.IMAGE_QUALITY_HIGH)
+logo = wx.Bitmap(img)
+logo_widget = wx.StaticBitmap(panel, -1, logo, (10, 5), (logo.GetWidth(), logo.GetHeight()))
+header.Add(logo_widget, wx.ALL|wx.RIGHT, 5)
 
-class PVDropdownList(wx.Choice, PVWidget):
+vbox.Add(header)
 
-    def __init__(self, parent, pv, writetopv=None, condition=lambda x: x!="", onSelection=None, **kw):
-
-        cargs = PVWidget.processArgs(kw)
-        PVWidget.__init__(self, pv, default=cargs['default'])
-        wx.Choice.__init__(self, parent, id=wx.ID_ANY, **kw)
-        self.condition = condition
-        self.writetopv = writetopv
-
-        if onSelection: self.Bind(wx.EVT_CHOICE, onSelection)
-        else: self.Bind(wx.EVT_CHOICE, self.onSelection)
-
-    def updateContent(self):
-        wx.Choice.Clear(self)       
-        for choice in list(filter(self.condition, self.value)): wx.Choice.Append(self, str(choice))
-
-    def onSelection(self, event):
-        if self.writetopv: self.writetopv.put(event.GetEventObject().GetStringSelection())
-
-    def setWriteToPV(self, writetopv):
-        self.writetopv = writetopv
+# get the list of monitors
+monitor_pv = epics.PV("%s:LIST:MONITOR" % prefix)
+if monitor_pv.wait_for_connection() == False: 
+    print "Cannot connect to PVs"
+    sys.exit(1)
 
 
-class PiCaMonApp(wx.Frame):
 
-    def __init__(self, prefix):
+for monitor in list(filter(lambda x: x!="", monitor_pv.get())):
 
-        self.prefix = prefix
+    hbox = wx.BoxSizer(wx.HORIZONTAL)
 
-        # prepare frame + main panel
-        wx.Frame.__init__(self, None, wx.ID_ANY, "PiCaMon", size=(300, 150))
-        panel = wx.Panel(self, wx.ID_ANY) 
+    three = wx.StaticText(panel, label = monitor, style = wx.ALIGN_CENTRE)
+    three.SetFont(boldfont)
+    hbox.Add(three, 0, wx.ALL|wx.CENTER, 5)
 
-        # create PV objects
-        monitors_pv = epics.PV("%s:LIST:MONITOR" % self.prefix)
-        cameras_pv = epics.PV("%s:LIST:CAMERA" % self.prefix)
+    five = wx.Choice(panel, id=wx.ID_ANY, name=monitor)
+    five.SetFont(normalfont)
+    five.Bind(wx.EVT_CHOICE, onCameraSelection)
+    camera_pv = epics.PV("%s:LIST:CAMERA" % prefix)
+    for camera in list(filter(lambda x: x!="", camera_pv.get())): five.Append(str(camera))
+    hbox.Add(five, 0, wx.ALL|wx.CENTER, 5)
 
-        # create wx widgets
-        self.monitors = PVDropdownList(panel, monitors_pv, onSelection=self.onMonitorSelection)
-        self.cameras = PVDropdownList(panel, cameras_pv)
+    four = epics.wx.PVText(panel, pv="%s:%s:CAMERA" % (prefix, monitor), fg=wx.Colour(0,0,255))
+    four.SetFont(smallfont)
 
-        # place in UI layout
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        
-        monitors_list = monitors_pv.get()
-        self.selected_pv = {}
-        for monitor in list(filter(lambda x: x!="", monitors_list)):
-            self.selected_pv[monitor] = epics.PV("%s:%s:CAMERA" % (self.prefix, monitor))
+    epics.poll()
+    hbox.Add(four, 0, wx.ALL|wx.CENTER, 5)
 
-        # display the list of monitor/camera pairs
-        for monitor in list(filter(lambda x: x!="", monitors_list)):
-            hbox = wx.BoxSizer(wx.HORIZONTAL)
-            st = wx.StaticText(panel, label = monitor, style = wx.ALIGN_CENTRE)
-            hbox.Add(st, 0, wx.ALL|wx.CENTER, 5) 
-            tu = PVTextUpdate(panel, self.selected_pv[monitor])  
-            hbox.Add(tu, 0, wx.ALL|wx.CENTER, 5)
-            vbox.Add(hbox, 1, wx.ALL|wx.EXPAND)
+    vbox.Add(hbox, 1, wx.ALL|wx.EXPAND)
 
-        # dropdown lists to select monitor/camera. 
-        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-        hbox2.Add(self.monitors, 0, wx.ALL|wx.CENTER, 5)
-        hbox2.Add(self.cameras, 0, wx.ALL|wx.CENTER, 5)
-        vbox.Add(hbox2, 1, wx.ALL|wx.EXPAND)
-        
-        panel.SetSizer(vbox)    
+panel.SetSizer(vbox)
 
-    def onMonitorSelection(self, event):
-        monitor = event.GetEventObject().GetStringSelection()
-        self.cameras.setWriteToPV(self.selected_pv[monitor])
+frame.Show()
+app.MainLoop()
 
 
-if __name__ == '__main__':
-    
-    app = wx.App()
-    PiCaMonApp("D100X").Show()
-    app.MainLoop()
  
